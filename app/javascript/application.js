90b289442b7b985d994e0bc3fa0cc7b53e3eb695
denvermullets <devmullets@gmail.com>
2026-04-01 18:16:47 -0400

    fix(safari): resolve turbo prefetch

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.config.drive.prefetchEnabled = false
 import * as ActiveStorage from "@rails/activestorage"
 ActiveStorage.start()
 import "controllers"
