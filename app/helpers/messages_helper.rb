17fffcbeed85c4006a802a1c2862811c04a73e74
denvermullets <devmullets@gmail.com>
2026-04-02 16:38:23 -0400

    fix(messaging): group messages w/in 60s

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,4 +1,6 @@
 module MessagesHelper
+  GROUP_WINDOW = 60.seconds
+
   def sanitize_message(html)
     return '' if html.blank?
 
@@ -10,5 +12,13 @@ module MessagesHelper
 
     highlighted = Messages::HighlightService.call(html: sanitized)
     Mentions::HighlightService.call(html: highlighted, current_user: Current.user)
+  end
+
+  def grouped_with_previous?(message, previous_message)
+    return false if previous_message.nil?
+    return false if previous_message.deleted?
+
+    previous_message.user_id == message.user_id &&
+      (message.created_at - previous_message.created_at) <= GROUP_WINDOW
   end
 end
