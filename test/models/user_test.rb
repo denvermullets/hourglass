e56f29047416a309523bca3578da98f3dd6beb98
denvermullets <devmullets@gmail.com>
2026-07-09 06:49:28 -0400

    fix: add server notification on new messages

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -43,4 +43,32 @@ class UserTest < ActiveSupport::TestCase
     assert_not user.valid?
     assert user.errors[:bio].any?
   end
+
+  test 'unread_server_ids is empty when no channel has messages' do
+    assert_empty users(:two).unread_server_ids
+  end
+
+  test 'unread_server_ids includes a server whose channel was never read' do
+    channels(:general).update!(last_message_at: Time.current)
+    assert_equal Set[servers(:one).id], users(:two).unread_server_ids
+  end
+
+  test 'unread_server_ids excludes a server whose channels are all read' do
+    channels(:general).update!(last_message_at: 1.hour.ago)
+    ChannelMembership.create!(user: users(:two), channel: channels(:general), last_read_at: Time.current)
+    assert_empty users(:two).unread_server_ids
+  end
+
+  test 'unread_server_ids includes a server read before the latest message' do
+    ChannelMembership.create!(user: users(:two), channel: channels(:general), last_read_at: 1.hour.ago)
+    channels(:general).update!(last_message_at: Time.current)
+    assert_equal Set[servers(:one).id], users(:two).unread_server_ids
+  end
+
+  test 'unread_server_ids ignores private channels the user cannot see' do
+    private_channel = servers(:one).channels.create!(name: 'secret', is_private: true, last_message_at: Time.current)
+    ChannelMembership.create!(user: users(:one), channel: private_channel)
+    assert_empty users(:two).unread_server_ids
+    assert_equal Set[servers(:one).id], users(:one).unread_server_ids
+  end
 end
